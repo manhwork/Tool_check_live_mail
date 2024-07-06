ced39b7e881bf846d7b7ca30c0da6fd4c7364d06
Anony Nguyễn <135792849+manhwork@users.noreply.github.com>
2024-07-06 11:24:50 +0700

    Add files via upload

---
+++ tool_check_live_mail/tool_check_mail.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import time
+
+
+options = webdriver.ChromeOptions()
+# options.add_argument("user-data-dir-"+ acc)
+options.add_experimental_option('detach',True)
+service = Service(executable_path="./chromedriver.exe")
+
+f1 = open("C:\\Users\\MTQV\\Desktop\\Python Program\\tool_check_live_mail\\hotmail.txt","r")
+accs = f1.read().split("\n")
+f1.close()
+print(accs)
+
+for acc in accs:
+    driver = webdriver.Chrome(options=options,service=service)
+    driver.get("https://login.live.com/")
+    time.sleep(5)
+    info = acc.split("|")
+    print(info)
+    user = info[0]
+    passw = info[1]
+    
+    driver.find_element(By.ID,value='i0116').send_keys(user)
+    time.sleep(5)
+    driver.find_element(By.ID,value='idSIButton9').click()
+    time.sleep(5)
+
+    driver.find_element(By.ID,value='i0118').send_keys(passw)
+    time.sleep(5)
+    driver.find_element(By.ID,value='idSIButton9').click()
+    time.sleep(5)
+
+    # driver.quit()
+    
+    
